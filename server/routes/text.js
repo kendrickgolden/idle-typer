f5a33277adbd8a4e06fa6eedec8f19815263a2fc
kendrickgolden <kendrickgolden24@gmail.com>
2022-08-24 22:00:03 -0500

    added api route for retrieving text

---
+++ server/routes/text.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const Text = require('../models/text');
+
+router.get('/', async function(req,res) {
+    const text = Text.findOne({ 'title' : req.query.title }, async (error,data) => {
+        if(error){
+            console.log("Could not find text:" + error);
+    } else {
+        res.json(data);
+       
+    }});
+    
+   // res.json("test");
+
+});
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,7 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const textRouter = require('./routes/text');
+
 const app = express();
+
 const mongoDB = process.env.ATLAS_URI;
 mongoose.connect(mongoDB);
 const db = mongoose.connection;
@@ -12,10 +15,18 @@ db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 const port = process.env.PORT || 5000;
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, OPTIONS');
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
- 
+app.use('/api/text', textRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
