c2aadce40e2c21b0bb275bdba9d1a3a18a4bbcb3
kendrickgolden <kendrickgolden24@gmail.com>
2022-09-08 18:58:24 -0500

    fixed text reading html entities

--- server/modules/parse-html.js
+++ server/modules/parse-html.js
@@ -18,19 +18,26 @@ fs.readFile(file, 'utf8', async (err,raw_text) => {
         console.error(err);
         return;
     }
+    
     //Parse text and create an array of chapters
     const chapter_regex = /<div class="chapter">(.*?)<\/div>/gs; 
     const chapter_title_regex = /(?<=<\/a>).*(?=<\/h2>)/g;
     const chapter_text_regex =/(?<=<p.*>)(((?!<p.*>).)*)?(?=<\/p>)/gs;
     
     const chapters = raw_text.match(chapter_regex);
+   
     const chapter_objs = chapters.map(chapter => ({ title: chapter.match(chapter_title_regex).toString(), paragraphs: chapter.match(chapter_text_regex)}));
+    
 
+    console.log(chapter_objs[0]);
     for(const chapter_obj of chapter_objs) {
-       chapter_obj.paragraphs = chapter_obj.paragraphs.map(p => p.replace(/\r\n/g, ' '));
-       chapter_obj.paragraphs = chapter_obj.paragraphs.map(p => p.replace(/<.*?>/gs, ''));
+     //  chapter_obj.paragraphs = chapter_obj.paragraphs.map(p => p.replace(/\r\n/g, ' '));
+        chapter_obj.paragraphs = chapter_obj.paragraphs.map(p => p.replace(/\n/g, ' '));
+        chapter_obj.paragraphs = chapter_obj.paragraphs.map(p => p.replace(/<.*?>/gs, ''));
+        chapter_obj.paragraphs = chapter_obj.paragraphs.map(p => p.replace(/—/, '-'));
     }
 
+    console.log(chapter_objs[0]);
     //Add text to mongoose database
     const text = await Text.create({title: title, author: author, chapters: chapter_objs});
 });
